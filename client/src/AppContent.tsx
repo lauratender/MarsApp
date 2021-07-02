59efee2cb5e0a307e568df9fb27d22308d4fb692
Laura Tender <lau.tender@gmail.com>
2021-07-02 17:29:46 +0300

    Added camera types select

--- client/src/AppContent.tsx
+++ client/src/AppContent.tsx
@@ -1,51 +1,101 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Select from 'react-select';
-import { FullRover } from './models/rovers';
+import Select, {ValueType} from 'react-select';
+import { Rover } from './models/rovers';
+import { extractDataFromRovers, extractCamerasFromRovers } from './models/rovers'
 
 interface Option {
     value: string,
-    label: string
+    label: string,
+    isdisabled: boolean
 }
 
 interface AppContextType {
-    setRovers: (rovers: FullRover[]) => void;
-    rovers: FullRover[];
+    setRovers: (rovers: Map<string, Rover>) => void;
+    rovers: Map<string, Rover>;
 }
 
 export const AppContext = React.createContext<AppContextType>({
-    rovers: [],
+    rovers: new Map(),
     setRovers: () => {},
 })
 
 function AppContent() {
-    const [rovers, setRovers] = useState<FullRover[]>([]); 
+    const [rovers, setRovers] = useState<Map<string, Rover>>(new Map()); 
+    const [selectedRoverOption, setSelectedRoverOption] = useState<ValueType<Option, false>>(null);
+    const [selectedOption, setSelectedOption] = useState<ValueType<Option, false>>(null);
+    const [cameras, setCameras] = useState<string[]>([]);
+    let test: string = "null";
 
     useEffect(() => {
         axios.get("/rovers")
             .then((response) => {
-                setRovers(response.data.rovers);
+                const roversResp = response.data.rovers
+                setRovers(extractDataFromRovers(roversResp));
+                setCameras(extractCamerasFromRovers(roversResp));
             })
             .catch(error => {
                 console.log(error);
             });
     }, []);
 
-    function getOptions(): Option[]{
+    function getRoverOptions(): Option[]{
         const options: Option[] = [];
-        for (let i = 0; i < rovers.length; i++){
+        rovers.forEach((value: Rover, key: string) => {
             const option: Option = {
-                value: rovers[i].name,
-                label: rovers[i].name
+                value: key,
+                label: key,
+                isdisabled: false
+            }
+            options.push(option);
+        });
+        return options;
+    }
+
+    function getCameraOptions(): Option[]{
+        const options: Option[] = [];
+        
+        let selectedRover = null;
+        if (selectedRoverOption)
+            selectedRover = rovers.get(selectedRoverOption.value)
+            
+
+        for (let i = 0; i < cameras.length; i++){
+
+            let isDisabled: boolean = true;
+            if (selectedRover && (selectedRover.cameras.includes(cameras[i])))
+                isDisabled = false;
+
+            const option: Option = {
+                value: cameras[i],
+                label: cameras[i],
+                isdisabled: isDisabled
             }
             options.push(option);
         }
         return options;
     }
 
+    function handleChange(option: ValueType<Option, false>): void {
+        setSelectedRoverOption(option);
+        getCameraOptions();
+    }
+
     return (
         <AppContext.Provider value = {{rovers, setRovers}}>
-            <Select options={getOptions()} />
+            <Select 
+                placeholder="Select Rover" 
+                options={getRoverOptions()} 
+                value = {selectedRoverOption} 
+                onChange={handleChange}
+            />
+            <Select 
+                placeholder="Select Camera" 
+                options={getCameraOptions()}
+                value = {selectedOption} 
+                onChange={setSelectedOption}
+                isOptionDisabled={(option) => option.isdisabled}
+            />
         </AppContext.Provider>
     );
 }
--- client/src/models/rovers.tsx
+++ client/src/models/rovers.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Camera } from "./camera"
 
-export interface FullRover {
+interface FullRover {
     id: number;
     name: string;
     landing_date: string;
@@ -12,4 +12,41 @@ export interface FullRover {
     max_date: string;
     total_photos: number;
     cameras: Camera[];
+}
+
+export interface Rover{
+    id: number;
+    name: string;
+    cameras: string[]
+}
+
+export function extractDataFromRovers(rovers: FullRover[]): Map<string, Rover>{
+    let resRoverMap = new Map();
+    for (let i = 0; i < rovers.length; i++){
+        const roverName = rovers[i].name;
+
+        const cameras: string[] = [];
+        for (let camera of rovers[i].cameras){
+            cameras.push(camera.name);
+        }
+
+        const rover: Rover = {
+            id: rovers[i].id,
+            name: rovers[i].name,
+            cameras: cameras
+        }
+        resRoverMap.set(roverName, rover);
+    }
+    return resRoverMap;
+}
+
+export function extractCamerasFromRovers(rovers: FullRover[]): string[] {
+    const cameras = new Set<string>();
+    for (let i = 0; i < rovers.length; i++){
+        for (let camera of rovers[i].cameras){
+            cameras.add(camera.name);
+        }
+    }
+    const resCameras: string[] = Array.from(cameras);
+    return resCameras;
 }
